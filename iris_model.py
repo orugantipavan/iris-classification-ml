c69de73487af463ce786a3ea6fdc2c2f1fa72237
Oruganti Pavan <orugantipavan21@gmail.com>
2026-03-14 18:40:09 +0530

    Added Iris classification ML project

---
+++ iris_model.py
@@ -0,0 +1,20 @@
+# Import Libraries
+import pandas as pd
+import numpy as np 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuray_score
+# Load Dataset
+iris = load_iris()
+X, y = iris.data, iris.target
+# Split Dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+# Train Model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+# Predict
+predictions = model.predict(X_test)
+# Accurary
+accuracy=accuracy_score(y_test, predictions)
+print("Model Accuracy:", accuracy)
