af58c4905819fc8ac45c95d7eece4f4d4b5c8e2a
orugantipavan <orugantipavan21@gmail.com>
2026-03-16 15:59:54 +0530

    Update iris_model.py

--- iris_model.py
+++ iris_model.py
@@ -1,19 +1,23 @@
 # Import Libraries
-import numpy as np 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuray_score
+
 # Load Dataset
 iris = load_iris()
 X, y = iris.data, iris.target
+
 # Split Dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
 # Train Model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
+
 # Predict
 predictions = model.predict(X_test)
+
 # Accuracy
 accuracy=accuracy_score(y_test, predictions)
 print("Model Accuracy:", accuracy)
